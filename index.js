986b4e26c68bc1a73f6637477ea6f85991fc83d2
Goochy12-Laptop <liamgooch@live.com.au>
2020-11-25 16:01:13 +1100

    add working test picklists

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const path = require('path');
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname + '/script.js'));
+});
+
+app.get('/selectize.js', (req, res) => {
+    res.sendFile(path.join(__dirname + '/selectize.js'));
+});
+
+app.get('/selectize.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/selectize.css'));
 });
 
 app.listen(port, ()=>{
---
+++ script.js
@@ -0,0 +1,19 @@
+$('#categorySelect').selectize({
+    maxItems: 1,
+    placeholder: "Select a Category..."
+});
+
+$('#subcategorySelect').selectize({
+    maxItems: 1,
+    placeholder: "Select a Sub Category..."
+});
+
+$('#positionSelect').selectize({
+    placeholder: "Select a Position...",
+    maxItems: null
+});
+
+$('#taskSelect').selectize({
+    placeholder: "Select a Task...",
+    maxItems: null
+});
